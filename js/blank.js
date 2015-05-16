eae6235ec1c0a0c935595ce748da03ddb6845f5a
Erik <erik.langenborg@gmail.com>
2015-05-16 14:59:35 -0400

    On very large results (>2MB), place the data in an html page with a textarea

--- js/blank.js
+++ js/blank.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var csvElm = document.getElementById('csv');
+
+  var sizeElm = function(){
+    csvElm.style.height = (window.innerHeight - 45) + 'px';
+  }
+
+  window.addEventListener('resize', sizeElm);
+
+  csvElm.onclick = function(elm){
+    csvElm.focus();
+    csvElm.select();
+  }
+
   chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.action === 'csv') {
       csvElm.textContent = msg.data;
+      sizeElm();
     }
   });
 });
